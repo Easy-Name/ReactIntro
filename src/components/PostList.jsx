01162e293be4f1a0477a4f14a4c2f7e812b680b3
oluwaseyi.longe <oluwaseyi.longe@consorciomagalu.com.br>
2025-01-06 11:20:13 -0300

    clickable posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,52 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
-import NewPost from './NewPost';
 import Post from './Post';
-import Modal from './Modal';
 import classes from './PostList.module.css';
 
-function PostList(props) {
-  const [posts, setPosts] = useState([]);
-  
-  useEffect(() => {
-    async function fetchPosts() {
-      const response = await fetch('http://localhost:8080/posts');
-      const resData = await response.json();
-      setPosts(resData.posts);
-    }
-
-    fetchPosts();
-  }, []);
-
-  function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
-
-  let modalContent;
-
-  if (props.isPosting) {
-    modalContent = (
-      <Modal onClose={props.onStopPosting}>
-        <NewPost onCancel={props.onStopPosting} onAddPost={addPostHandler} />
-      </Modal>
-    );
-  }
+function PostList() {
+  const posts = useLoaderData();
 
   return (
     <>
-      {modalContent}
-
       {posts.length > 0 ? (
         <ul className={classes.posts}>
           {posts.map((post) => (
-            <Post key={post.body} author={post.author} body={post.body} />
+            <Post key={post.id} author={post.author} body={post.body} id = {post.id} />
           ))}
         </ul>
       ) : (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Posts, { loader as postsLoader } from './routes/Posts';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NewPost, { action as newPostAction } from './routes/NewPost';
+import PostDetails, { loader as detailsLoader } from './routes/PostDetails';
+import RootLayout from './routes/RootLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element: <PostDetails /> , loader: detailsLoader},
+        ],
+      },
+      {},
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
